3dc4b2acba97ca5464a1b363d22203a58d4a1427
Navneet Guglani <Navneetguglanikhms@gmail.com>
2024-11-16 13:27:40 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Navneet Guglani',
+    author_email= 'navneetguglani2004@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
